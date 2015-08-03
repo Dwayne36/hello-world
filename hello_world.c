6561f917605bc20e8a96834b8c7551adb95f9916
dwayne <you@example.com>
2015-08-03 22:56:21 +1000

    init commit

---
+++ hello_world.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static int number;
+#define HW_STRING "hello world\n"
+
+int main(int argc, char **argv) {
+        char *string;
+	        number=7;
+		string = malloc(strlen(HW_STRING));
+				        
+		printf("String virtual address %lX\n", (unsigned long) string);
+						      
+		strcpy(string, HW_STRING);
+		printf("%s %i\n", string, number);
+		 return 0;
+ } 
